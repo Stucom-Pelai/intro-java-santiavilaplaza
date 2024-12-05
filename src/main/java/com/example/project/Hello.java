89958cf1ffe3225c7e3c12bf44e2e6a7fcec7679
santiavilaplaza <94676125+santiavilaplaza@users.noreply.github.com>
2024-12-05 19:35:30 +0100

    añadir \n

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -2,7 +2,7 @@ package com.example.project;
 
 public class Hello { 
 	public static void main(final String[] args) {
-        	System.out.print("Hello world!");
+        	System.out.print("Hello world!\n");
 	}
 
 }
